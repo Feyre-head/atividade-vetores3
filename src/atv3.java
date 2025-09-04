c73f61c868bb27d5387bf8aff9bb7a6edc63cca9
Feyre <inaraoenning@gmail.com>
2025-09-04 20:15:03 -0300

    Add: atv 5 / finalizando atv 3/4

--- src/atv3.java
+++ src/atv3.java
@@ -2,20 +2,21 @@ public class atv3 {
     public static void main(String[] args) {
         //manipulação direta dos elementos de um vetor
 
-        int [] listaVetor2 = {5,10};
+        int[] listaVetor2 = {5, 10};
 
         System.out.println("Lista Inicial Vetor");
 
-        for (int i = 0; i<2; i++){
-            System.out.println("Index "+ (i) + ": "+ listaVetor2[i]);
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Index " + (i) + ": " + listaVetor2[i]);
         }
 
         listaVetor2[0] = 10;
-        listaVetor2[1]= 5;
+        listaVetor2[1] = 5;
 
         System.out.println("Valor index alterado");
-        for (int num: listaVetor2){
-            System.out.println("Index " + (i));
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Index " + (i + 1) + listaVetor2[0]);
+            System.out.println("Index " + (i + 1) + listaVetor2[1]);
         }
 
 
--- src/atv4.java
+++ src/atv4.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class atv4 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] vetorStorage = new int[3];
+
+        System.out.println("Vamos armazenar 3 números em um vetor:");
+        int soma = 0;
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Insira o " + (i + 1) + "º Número:");
+            vetorStorage[i] = sc.nextInt();
+            soma += vetorStorage[i];
+        }
+
+        System.out.println("Números Armazenados no Vetor:");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(vetorStorage[i]);
+        }
+
+        int media = soma / 3;
+
+        System.out.println("Soma dos Números Armazenados:" + soma);
+        System.out.println("Média dos Números Armazenados:" + media);
+        System.out.println("==================FIM==================");
+    }
 }

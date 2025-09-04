743160d714fec70d1a891c827945761ccc88c82e
Feyre <inaraoenning@gmail.com>
2025-09-04 20:51:24 -0300

    Add:atv 7 / finalizando atv 6

--- src/atv6.java
+++ src/atv6.java
@@ -7,10 +7,22 @@ public class atv6 {
 
         int[] listaInteiros = new int[10];
 
+        System.out.println("===============================================");
         System.out.println("Adicione 10 números para saber quais sao pares");
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("Informe o " + (i + 1) + " º");
+            System.out.println("Informe o " + (i + 1) + " º :");
+            listaInteiros[i] = sc.nextInt();
         }
+
+        System.out.println("====Números Pares====");
+        for (int i = 0; i < 10; i++) {
+            if (listaInteiros[i] % 2 == 0) {
+                System.out.println(listaInteiros[i]);
+            }
+        }
+        System.out.println("===============================================");
+
+        sc.close();
     }
 }
---
+++ src/atv7.java
@@ -0,0 +1,5 @@
+public class atv7 {
+    public static void main(String[] args) {
+        //ler 15 nomes armazenar e printar ao contrario
+    }
+}

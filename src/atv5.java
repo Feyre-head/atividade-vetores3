1db561965834ca677f73bd5c13124448764c6860
Feyre <inaraoenning@gmail.com>
2025-09-04 20:24:04 -0300

    Add: atv6/ finalizando atv 5

--- src/atv5.java
+++ src/atv5.java
@@ -1,3 +1,23 @@
-public class atv5 {
+import java.util.Scanner;
 
+public class atv5 {
+    //Scanner para ler nomes inseridos pelo usuário e armazenar em um Vetor
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] listaNomes = new String[3];
+
+        System.out.println("====Vamos Armazenar 3 Nomes====");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Insira o " + (i + 1) + " nome:");
+            listaNomes[i] = sc.next();
+        }
+
+        System.out.println("======Imprimindo Nomes======");
+        for (int i = 0; i < 3; i++) {
+            System.out.println((i + 1) + "º nome:" + listaNomes[i]);
+        }
+        System.out.println("============FIM=============");
+
+    }
 }
---
+++ src/atv6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class atv6 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] listaInteiros = new int[10];
+
+        System.out.println("Adicione 10 números para saber quais sao pares");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Informe o " + (i + 1) + " º");
+        }
+    }
+}
